48588be4934bf4ff3673a296f51a2ea4db35a70a
MSaad-AlphaSquad <saad.alphasquad@gmail.com>
2021-04-23 17:21:39 +0500

    Countdown v1

---
+++ components/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Form = ({name, onFormSubmit, date, onInputChange}) => {
+    return (<div>
+        <form onSubmit={onFormSubmit} className="grid grid-cols-1">
+        <label className="mb-2">Enter Event Name</label>
+        <input value={name} name='name' onChange={onInputChange} className="mb-3"/>
+        <label className="mb-2">Enter Data</label>
+        <input className="mb-3" type="date" value={date} name='date' onChange={onInputChange} name="date"/>
+        <button type="submit">Start</button>
+        
+    </form>
+    <hr className="border-1 mx-2 my-10 border-gray-400 "></hr>
+    </div>
+    )
+}
+
+
+export default Form;
---
+++ components/Timer.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Timer = ({months, days, hours, minutes, seconds}) => {
+  return (
+      <div>
+    <div className="flex flex-row space-x-5 text-xs">
+      <div>
+        <h1>{months}</h1>
+        <p>months</p>
+      </div>
+      <div>
+        <h1>{days}</h1>
+        <p>days</p>
+      </div>
+      <div>
+        <h1>{hours}</h1>
+        <p>hours</p>
+      </div>
+      <div>
+        <h1>{minutes}</h1>
+        <p>minutes</p>
+      </div>
+      <div>
+        <h1>{seconds}</h1>
+        <p>seconds</p>
+      </div>
+    </div>
+    <hr className="border-1 mx-2 my-10 border-gray-400 "></hr>
+    </div>
+  );
+};
+
+
+export default Timer;
